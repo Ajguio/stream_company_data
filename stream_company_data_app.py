693758922a454240338d910b3516b92a096e3ae3
Ajguio <150486535+Ajguio@users.noreply.github.com>
2024-12-08 14:32:08 -0500

    Update stream_company_data_app.py

--- stream_company_data_app.py
+++ stream_company_data_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
-streamlit.title("My Mom's New Healthy Diner")
+# Título principal
+streamlit.title('My Parents New Healthy Diner')
+
+# Encabezado del menú
+streamlit.header('Breakfast Menu')
+
+# Elementos del menú
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

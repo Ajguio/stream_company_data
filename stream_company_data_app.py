0bb289940c1e3eab67c355cb0e76829c688b7a73
Ajguio <150486535+Ajguio@users.noreply.github.com>
2024-12-08 14:34:48 -0500

    Update stream_company_data_app.py

--- stream_company_data_app.py
+++ stream_company_data_app.py
@@ -10,3 +10,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read_csv("https://glchallenge.s3.us-west-1.amazonaws.com/filesemployees/hired_employees/hired_employees.csv")
+
